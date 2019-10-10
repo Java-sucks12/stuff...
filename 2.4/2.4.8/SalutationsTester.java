2f42f6b66b0915d6188766ae098b7f29a6c59f36
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2019-10-10 08:59:03 -0500

    Create SalutationsTester.java

---
+++ 2.4/2.4.8/SalutationsTester.java
@@ -0,0 +1,12 @@
+public class SalutationsTester
+{
+    public static void main(String[] args)
+    {
+        // Test your class here
+        Salutations human =  new Salutations("name");
+        human.addressLetter();
+        human.signLetter();
+        human.addressMemo();
+        human.signMemo();
+    }
+}
