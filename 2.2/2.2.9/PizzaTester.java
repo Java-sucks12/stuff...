caeef161b45ce3ea60e7706eea613f0bf9e24cc8
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2019-10-09 21:23:47 -0500

    Create PizzaTester.java

---
+++ 2.2/2.2.9/PizzaTester.java
@@ -0,0 +1,13 @@
+public class PizzaTester 
+{
+    public static void main(String[] args)
+    {
+        // Test your Pizza class here.
+        Pizza food = new Pizza("tasty", "ice", 35);
+        Pizza food2 = new Pizza("jksbf", "jkbfa", 99);
+        Pizza food3 = new Pizza("nfsf", "ekjsnfl", 2000);
+        System.out.println(food);
+        System.out.println(food2);
+        System.out.println(food3);
+    }
+}
