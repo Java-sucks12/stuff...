5a9929f3d679f016349421115f56174b9d93f1f8
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2019-10-09 21:08:17 -0500

    Create Pizza.java

---
+++ 2.1.8/Pizza.java
@@ -0,0 +1,11 @@
+public class Pizza
+{
+    // Add your instance variables here
+    int pepperoni;
+    int pineapple;
+    int otherToppings;
+    
+    public Pizza(int pepperoni, int pineapple){
+        
+    }
+}
