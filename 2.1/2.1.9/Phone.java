4af946fc46ef9fab0b1fa219e2e14b9a9648e629
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2019-10-09 21:11:48 -0500

    Update and rename 2.1/test to 2.1/2.1.9/Phone.java

---
+++ 2.1/2.1.9/Phone.java
@@ -0,0 +1,24 @@
+
+/**
+ * 
+ * The phone class represents a cellular phone.
+ * 
+ * Add the instance variables you think a phone would need
+ */ 
+
+public class Phone
+{
+
+    // Attributes
+    String contacts;
+    int savedNumbers;
+    int blockedNumbers;
+    
+    // Methods will go here
+    public phone(String phoneContacts, int phoneSavedNumbers, int phoneBlockedNumbers){
+        contacts = phoneContacts;
+        savedNumbers = phoneSavedNumbers;
+        BlockedNumbers = phoneBlockedNumbers;
+        
+    }
+}
