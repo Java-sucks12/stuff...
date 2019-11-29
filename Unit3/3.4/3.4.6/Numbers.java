4b2e6755c4d5d4dea14a29fb98719f1fd99e188a
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2019-11-29 15:40:24 -0600

    Create Numbers.java

---
+++ Unit3/3.4/3.4.6/Numbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Numbers
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        if (num < 0){
+            System.out.println("The number is negative!");
+        }
+        else if (num > 0){
+            System.out.println("The number is positive!");
+        }
+        else{
+            System.out.println("The number is neither positive nor negative!");
+        }
+    }
+}
