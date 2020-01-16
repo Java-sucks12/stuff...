b6b130e8e929d27e76f101f948fa76c405120c46
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2020-01-16 09:00:42 -0600

    Create DNA.java

---
+++ Unit 5/5.1/5.1.4/DNA.java
@@ -0,0 +1,11 @@
+public class DNA
+{
+    private String rsid;
+    private String genotype;
+    
+    public DNA(String rsids, String genotypes){
+        rsid = rsids;
+        genotype = genotypes;
+    }
+    
+}
