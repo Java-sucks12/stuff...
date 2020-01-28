299288e61f5e02c7b8e30fa20f55d82544483a98
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2020-01-28 09:23:11 -0600

    Create Employee.java

---
+++ Unit 5/5.1/5.1.5/Employee.java
@@ -0,0 +1,12 @@
+public class Employee
+{
+    private String name;
+    private int id;
+    private double salary;
+    
+    public Employee(String n, int i, double sal){
+        name = n;
+        id = i;
+        salary = sal;
+    }
+}
