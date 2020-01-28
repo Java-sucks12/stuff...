d22d0e386dd6868bb1ede91b57cf4005f0a34222
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2020-01-28 09:58:34 -0600

    Create Dragon.java

---
+++ Unit 5/5.4/5.4.7/Dragon.java
@@ -0,0 +1,49 @@
+public class Dragon 
+{
+    private String name;
+    private int level;
+    private boolean canBreatheFire;
+    
+    // Write the constructor here!
+    public Dragon(String nm, int lvl){
+        name = nm;
+        level = lvl;
+        if (level >= 70){
+            canBreatheFire = true;
+        }
+        else{
+            canBreatheFire = false;
+        }
+    }
+    // Put getters here
+    public String getName(){
+        return name;
+    }
+    public int getLevel(){
+        return level;
+    }
+    public boolean isFireBreather(){
+        return canBreatheFire;
+    }
+    // Put other methods here
+    public void attack(){
+        if (canBreatheFire){
+            System.out.println(">>>>>>>>>>\n>>>>>>>>>>>>>>\n>>>>>>>>>>>>>>\n>>>>>>>>>>");
+        }
+        else {
+            System.out.println("~ ~ ~");
+        }
+    }
+
+    public void gainExperience(){
+        level += 10;
+        if (level >= 70){
+            canBreatheFire = true;
+        }
+    }
+    // String representation of the object
+    public String toString()
+    {
+        return "Dragon " + name + " is at level " + level;
+    }
+}
