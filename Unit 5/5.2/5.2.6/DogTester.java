900916a8868f2fa330750af4896a1aab2b95ce5a
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2020-01-28 09:45:11 -0600

    Create DogTester.java

---
+++ Unit 5/5.2/5.2.6/DogTester.java
@@ -0,0 +1,12 @@
+public class DogTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Dog k = new Dog("Karel", 7, 15.6);
+        Dog c = new Dog("Clover", 9);
+        
+        System.out.println(k);
+        System.out.println(c);
+    }
+}
