6653d7654061a12b26fbadc82c87ad91b4dc9a0f
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2020-01-28 10:10:16 -0600

    Create SongTester.java

---
+++ Unit 5/5.9/5.9.6/SongTester.java
@@ -0,0 +1,13 @@
+public class SongTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Song s1 = new Song("a1", "t2", 10, 10);
+        Song s2 = new Song("a1", "t2", 10, 10);
+        Song s3 = new Song("a1", "t2", 10, 10);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
