a3d5cb785ed444725222e8b18d954434d5740ecf
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2020-02-07 09:01:10 -0600

    Create LastElement.java

---
+++ Unit6/6.1/6.1.8/LastElement.java
@@ -0,0 +1,15 @@
+public class LastElement
+{
+    public static void main(String[] args)
+    {
+        String[] arr = new String[]{"hello", "my name", "world", "Karel"};
+        //get and print the last element of the array
+        System.out.println("The last element of the String array is: " + getLastElement(arr));
+    }
+
+    public static String getLastElement(String[] arr)
+    {
+        // Your code goes here!
+        return arr[3];
+    }
+}
